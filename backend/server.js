84ca617d5e711ccd450a6db83d7ccc6aab8fd5da
Tarek Bouzo <tarekbozo@gmail.com>
2020-12-04 09:41:13 +0100

    production

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,20 @@ app.use('/api/products', productRouts);
 app.use(notFound);
 app.use(errorHandler);
 
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.NODE_ENV;
 
